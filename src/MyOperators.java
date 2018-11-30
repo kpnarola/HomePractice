2543bc089e7e441f0a1b77aa273209e3da9574ea
kpnarola <kpnarola@gmail.com>
2018-11-30 11:40:06 +0000

    hi

--- src/MyOperators.java
+++ src/MyOperators.java
@@ -3,5 +3,11 @@ public class MyOperators {
 
     public static void main(String[] args) {
         System.out.println("HI");
+
+        System.out.println("My name is Kiran");
+        int i =1;
+        int j = 2;
+
+        }
     }
 }
